257e1f11057dbf9466118bad96672e6c37a37b56
Muhammad Hilmi ASYROFI <mhilmiasyrofi@gmail.com>
2020-12-04 00:39:47 +0800

    add eval script

---
+++ cifar10/eval.sh
@@ -0,0 +1,15 @@
+python eval.py --model resnet18 \
+    --train-adversarial deepfool \
+    --test-adversarial autoattack \
+    --best-model \
+    --lr-schedule piecewise \
+    --norm l_inf \
+    --epsilon 8 \
+    --epochs 110 \
+    --attack-iters 10 \
+    --pgd-alpha 2 \
+    --fname auto \
+    --optimizer 'momentum' \
+    --weight_decay 5e-4 \
+    --batch-size 128 \
+    --BNeval 
