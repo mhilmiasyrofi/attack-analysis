ca5a9fba1b82224d993f59a8c3289edba2892ae7
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2020-12-11 06:27:22 +0000

    add more attack

--- cifar10/eval.sh
+++ cifar10/eval.sh
@@ -1,40 +1,8 @@
-# declare -a train=("original" "autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "jsma"  "newtonfool"  "pgd" "pixelattack" "squareattack")
-# declare -a test=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "jsma"  "newtonfool" "pgd" "pixelattack" "squareattack")
+# declare -a train=("original" "autoattack" "autopgd" "bim" "cw" "fgsm")
+declare -a train=("pgd" "squareattack" "deepfool" "newtonfool" "pixelattack" "spatialtransformation")
 
 
-# declare -a train=("original" "autoattack" "autopgd" "bim" "cw" "fgsm" "jsma" "pgd" "squareattack")
-# declare -a test=("autoattack" "autopgd" "bim" "cw" "fgsm" "jsma" "pgd" "squareattack")
-
-
-declare -a train=("original" "autoattack" "autopgd" "bim" "cw" "fgsm" "jsma" "pgd" "squareattack" "deepfool" "newtonfool" "pixelattack")
-declare -a test=("spatialtransformation")
-
-
-for tr in ${train[@]}; do
-    for ts in ${test[@]}; do
-        python eval.py --model resnet18 \
-            --train-adversarial $tr \
-            --test-adversarial $ts \
-            --best-model \
-            --lr-schedule piecewise \
-            --norm l_inf \
-            --epsilon 8 \
-            --epochs 110 \
-            --attack-iters 10 \
-            --pgd-alpha 2 \
-            --fname auto \
-            --optimizer 'momentum' \
-            --weight_decay 5e-4 \
-            --batch-size 256 \
-            --BNeval 
-    done
-done
-
-
-declare -a train=("spatialtransformation")
-
-declare -a test=("autoattack" "autopgd" "bim" "cw" "fgsm" "jsma" "pgd" "squareattack" "deepfool" "newtonfool" "pixelattack")
-
+declare -a test=("autoattack" "autopgd" "bim" "cw" "fgsm" "pgd" "squareattack" "deepfool" "newtonfool" "pixelattack" "spatialtransformation")
 
 
 for tr in ${train[@]}; do
