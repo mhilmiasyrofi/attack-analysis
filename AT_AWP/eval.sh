0fc6ca6c8ea6a9e8c15510a98919759130476d8b
Muhammad Hilmi ASYROFI <mhilmiasyrofi@gmail.com>
2020-12-15 21:55:02 +0800

    add evaluation procedure

---
+++ AT_AWP/eval.sh
@@ -0,0 +1,19 @@
+# declare -a train=("original" "autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+
+declare -a train=("pixelattack" "squareattack" "spatialtransformation")
+
+declare -a test=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+
+
+# Iterate the string array using for loop
+for tr in ${train[@]}; do
+    for ts in ${test[@]}; do
+        python eval_cifar10.py \
+            --train-adversarial $tr \
+            --test-adversarial $ts
+    done
+done
+
+# python eval_cifar10.py \
+#     --train-adversarial squareattack \
+#     --test-adversarial squareattack
