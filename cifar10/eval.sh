dbac95fd67c0ec1b750a4a9789b6f9e048ad841c
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2020-12-28 00:00:40 +0800

    edit run script

--- cifar10/eval.sh
+++ cifar10/eval.sh
@@ -1,29 +1,53 @@
-# declare -a train=("original" "autoattack" "autopgd" "bim" "cw" "fgsm")
-declare -a train=("pgd" "squareattack" "deepfool" "newtonfool" "pixelattack" "spatialtransformation")
+# declare -a train=("original" "autoattack" "autopgd" )
+# declare -a train=("bim" "cw" "fgsm")
+# declare -a train=("pgd" "squareattack" "deepfool")
+# declare -a train=("newtonfool" "pixelattack" "spatialtransformation")
+# declare -a train=("all")
 
+# declare -a test=("autoattack" "autopgd" "bim" "cw" "fgsm" "pgd" "squareattack" "deepfool" "newtonfool" "pixelattack" "spatialtransformation")
+
+
+# for tr in ${train[@]}; do
+#     for ts in ${test[@]}; do
+#         python eval.py --model resnet18 \
+#             --train-adversarial $tr \
+#             --test-adversarial $ts \
+#             --best-model \
+#             --lr-schedule piecewise \
+#             --norm l_inf \
+#             --epsilon 8 \
+#             --epochs 110 \
+#             --labelsmooth \
+#             --labelsmoothvalue 0.3 \
+#             --fname auto \
+#             --optimizer 'momentum' \
+#             --weight_decay 5e-4 \
+#             --batch-size 128 \
+#             --BNeval 
+#     done
+# done
 
 declare -a test=("autoattack" "autopgd" "bim" "cw" "fgsm" "pgd" "squareattack" "deepfool" "newtonfool" "pixelattack" "spatialtransformation")
 
-
-for tr in ${train[@]}; do
-    for ts in ${test[@]}; do
-        python eval.py --model resnet18 \
-            --train-adversarial $tr \
-            --test-adversarial $ts \
-            --best-model \
-            --lr-schedule piecewise \
-            --norm l_inf \
-            --epsilon 8 \
-            --epochs 110 \
-            --attack-iters 10 \
-            --pgd-alpha 2 \
-            --fname auto \
-            --optimizer 'momentum' \
-            --weight_decay 5e-4 \
-            --batch-size 256 \
-            --BNeval 
-    done
+for ts in ${test[@]}; do
+    python eval.py --model resnet18 \
+        --train-adversarial combine \
+        --list newtonfool_pixelattack_spatialtransformation_squareattack \
+        --test-adversarial $ts \
+        --best-model \
+        --lr-schedule piecewise \
+        --norm l_inf \
+        --epsilon 8 \
+        --epochs 110 \
+        --labelsmooth \
+        --labelsmoothvalue 0.3 \
+        --fname auto \
+        --optimizer 'momentum' \
+        --weight_decay 5e-4 \
+        --batch-size 128 \
+        --BNeval 
 done
+
 
 # python eval.py --model resnet18 \
 #     --train-adversarial spatialtransformation \
@@ -33,8 +57,8 @@ done
 #     --norm l_inf \
 #     --epsilon 8 \
 #     --epochs 110 \
-#     --attack-iters 10 \
-#     --pgd-alpha 2 \
+#     --labelsmooth \
+#     --labelsmoothvalue 0.3 \
 #     --fname auto \
 #     --optimizer 'momentum' \
 #     --weight_decay 5e-4 \
@@ -42,17 +66,18 @@ done
 #     --BNeval 
 
 # python eval.py --model resnet18 \
-#     --train-adversarial original \
+#     --train-adversarial combine \
+#     --list newtonfool_pixelattack_spatialtransformation_squareattack \
 #     --test-adversarial pgd \
 #     --best-model \
 #     --lr-schedule piecewise \
 #     --norm l_inf \
 #     --epsilon 8 \
 #     --epochs 110 \
-#     --attack-iters 10 \
-#     --pgd-alpha 2 \
+#     --labelsmooth \
+#     --labelsmoothvalue 0.3 \
 #     --fname auto \
 #     --optimizer 'momentum' \
 #     --weight_decay 5e-4 \
-#     --batch-size 256 \
+#     --batch-size 128 \
 #     --BNeval 
