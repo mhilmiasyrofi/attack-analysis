89628d6bc2e9dc7a746c75ca8e23a9441809020c
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-01-06 15:14:46 +0800

    add ensemble model script

---
+++ cifar10/ensemble_model.sh
@@ -0,0 +1,9 @@
+python ensemble_model.py  \
+    --model resnet18 \
+    --centroids pixelattack_spatialtransformation_autopgd \
+    --noise-predictor maxlr0.05_wd0.0001_ls0.3
+
+# python ensemble_model.py  \
+#     --model resnet18 \
+#     --centroids pixelattack_spatialtransformation_squareattack_autopgd \
+#     --noise-predictor maxlr0.05_wd0.0001_ls0.3
