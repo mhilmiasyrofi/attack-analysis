00efc47917cc1aa83edd7bd00549d324c2a2164c
Muhammad Hilmi ASYROFI <mhilmiasyrofi@gmail.com>
2021-01-22 08:24:25 +0800

    add evaluation

--- AT_AWP/eval.sh
+++ AT_AWP/eval.sh
@@ -4,7 +4,7 @@
 
 # declare -a train=("all")
 
-declare -a test=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+# declare -a test=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
 
 
 # Iterate the string array using for loop
@@ -12,16 +12,37 @@ declare -a test=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool
 #     for ts in ${test[@]}; do
 #         python eval_cifar10.py \
 #             --train-adversarial $tr \
-#             --test-adversarial $ts
+#             --test-adversarial $ts \
 #     done
 # done
 
-for ts in ${test[@]}; do
-    python eval_cifar10.py \
-        --train-adversarial combine \
-        --list autopgd_pixelattack_spatialtransformation_squareattack \
-        --test-adversarial $ts
+
+declare -a train=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+declare -a test=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+declare -a epochs=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+# declare -a train=("autoattack")
+# declare -a test=("autopgd")
+
+
+# Iterate the string array using for loop
+for ep in ${epochs[@]}; do
+    for tr in ${train[@]}; do
+        for ts in ${test[@]}; do
+            python eval_cifar10.py \
+                --train-adversarial $tr \
+                --test-adversarial $ts \
+                --model-epoch $ep \
+                --fname ../../trained_models/default/
+        done
+    done
 done
+
+# for ts in ${test[@]}; do
+#     python eval_cifar10.py \
+#         --train-adversarial combine \
+#         --list autopgd_pixelattack_spatialtransformation_squareattack \
+#         --test-adversarial $ts
+# done
 
 # python eval_cifar10.py \
 #     --train-adversarial squareattack \
