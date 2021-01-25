163f01aad5a601c7442430133509d5a7a452d361
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-01-25 09:49:05 +0800

    add latest commit

--- generate.sh
+++ generate.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
  
 
-# declare -a attack=("autoattack" "apgd" "boundaryattack" "brendelbethge" "cw" "deepfool" "elasticnet" "fgm" "hopskipjump" "bim" "pgd" "pixelattack" "thresholdattack" "jsma" "shadowattack" "spatialtransformation" "squareattack" "universalperturbation" "wasserstein" "zoo")
+# declare -a attack=("autoattack" "apgd" "boundaryattack" "brendelbethge" "cw" "deepfool" "elasticnet" "fgm" "hopskipjump" "bim" "pgd" "pixelattack" "thresholdattack" "jsma" "spatialtransformation" "squareattack" "universalperturbation" "wasserstein" "zoo")
 
 # declare -a attack=("boundaryattack" "brendelbethge" "cw")
 
 
 # declare -a attack=("autoattack" "autopgd" "squareattack")
 
-declare -a attack=("bim" "cw")
+# declare -a attack=("bim" "cw")
 
-# declare -a attack=("deepfool" "newtonfool" "spatialtransformation" "cw" "bim" "jsma" "elasticnet" "universalperturbation")
+# declare -a attack=( "cw" "bim" "jsma" "elasticnet" "universalperturbation")
 
-# declare -a attack=("bim" "jsma" "elasticnet" "universalperturbation")
+# declare -a attack=("jsma" "deepfool" "newtonfool" "spatialtransformation")
+
+declare -a attack=("pixelattack" "elasticnet" "universalperturbation")
+
 
 # Iterate the string array using for loop
 for a in ${attack[@]}; do
