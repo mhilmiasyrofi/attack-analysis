c0551f95ec28259e556b55ec254f6f9c24912719
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-01-27 18:11:36 +0800

    add validation set

--- Bag-of-Tricks-for-AT/train-rq1.sh
+++ Bag-of-Tricks-for-AT/train-rq1.sh
@@ -3,13 +3,17 @@
 ## docker run -it --rm --name gpu0-bot -v /home/mhilmiasyrofi/Documents/Bag-of-Tricks-for-AT/:/workspace/Bag-of-Tricks-for-AT/ --gpus '"device=0"' mhilmiasyrofi/advtraining
  
 ## Declare an array of string with type
-declare -a adv=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+# declare -a adv=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
 
 ## Used to run in different GPUs
 # declare -a adv=("autoattack" "autopgd" "bim")
 # declare -a adv=("cw" "deepfool" "fgsm")
 # declare -a adv=("newtonfool" "pgd" "pixelattack")
 # declare -a adv=("spatialtransformation" "squareattack")
+
+# declare -a adv=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm")
+declare -a adv=("newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+
 
 ## Iterate the string array using for loop
 for a in ${adv[@]}; do
@@ -21,9 +25,24 @@ for a in ${adv[@]}; do
         --epochs 110 \
         --labelsmooth \
         --labelsmoothvalue 0.3 \
-        --fname auto \
         --optimizer 'momentum' \
         --weight_decay 5e-4 \
         --batch-size 128 \
-        --BNeval 
+        --BNeval \
+        --val
 done
+
+# python adversarial_training.py --model resnet18 \
+#     --attack fgsm \
+#     --lr-schedule piecewise \
+#     --norm l_inf \
+#     --epsilon 8 \
+#     --epochs 110 \
+#     --labelsmooth \
+#     --labelsmoothvalue 0.3 \
+#     --fname auto \
+#     --optimizer 'momentum' \
+#     --weight_decay 5e-4 \
+#     --batch-size 128 \
+#     --BNeval \
+#     --val
