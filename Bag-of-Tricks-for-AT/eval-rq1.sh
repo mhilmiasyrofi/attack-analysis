4dd72e89e853ff955d173a3260128b64d8432729
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-01-28 10:21:17 +0800

    rq1-script

--- Bag-of-Tricks-for-AT/eval-rq1.sh
+++ Bag-of-Tricks-for-AT/eval-rq1.sh
@@ -1,28 +1,34 @@
-declare -a test=("autoattack" "autopgd" "bim" "cw" "fgsm" "pgd" "squareattack" "deepfool" "newtonfool" "pixelattack" "spatialtransformation")
-
 ## Used to run in different GPUs
 # declare -a train=("original" "autoattack" "autopgd" )
 # declare -a train=("bim" "cw" "fgsm")
 # declare -a train=("pgd" "squareattack" "deepfool")
 # declare -a train=("newtonfool" "pixelattack" "spatialtransformation")
-# declare -a train=("all")
+
+# declare -a train=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm")
+
+# declare -a train=("newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack" )
+
+
+declare -a train=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+
+declare -a test=("autoattack" "autopgd" "bim" "cw" "deepfool" "fgsm" "newtonfool" "pgd" "pixelattack" "spatialtransformation" "squareattack")
+
 
 for tr in ${train[@]}; do
     for ts in ${test[@]}; do
         python eval.py --model resnet18 \
             --train-adversarial $tr \
             --test-adversarial $ts \
-            --best-model \
-            --lr-schedule piecewise \
-            --norm l_inf \
-            --epsilon 8 \
-            --epochs 30 \
-            --labelsmooth \
-            --labelsmoothvalue 0.3 \
-            --fname auto \
-            --optimizer 'momentum' \
-            --weight_decay 5e-4 \
             --batch-size 128 \
-            --BNeval 
+            --model-dir ../trained_models/BagOfTricks/1000val/full/
     done
 done
+
+# python eval.py \
+#     --model resnet18 \
+#     --train-adversarial autoattack \
+#     --test-adversarial autopgd \
+#     --batch-size 128 \
+#     --model-dir ../trained_models/BagOfTricks/1000val/full/
+#     --val 1000 
+
