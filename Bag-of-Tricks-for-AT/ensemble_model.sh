55609a2aba69376affb2f994fba3d75345985841
Muhammad Hilmi ASYROFI <mhilmiasyrofi@gmail.com>
2021-02-04 10:52:29 +0800

    modify ensemble model script

--- Bag-of-Tricks-for-AT/ensemble_model.sh
+++ Bag-of-Tricks-for-AT/ensemble_model.sh
@@ -1,7 +1,6 @@
 python ensemble_model.py  \
     --model resnet18 \
-    --centroids pixelattack_spatialtransformation_autopgd \
-    --noise-predictor maxlr0.05_wd0.0001_ls0.3
+    --centroids pixelattack_spatialtransformation_autoattack
 
 # python ensemble_model.py  \
 #     --model resnet18 \
