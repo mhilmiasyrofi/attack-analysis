44c1022bad8460a3c753023297c7ff1eaf03a952
Muhammad Hilmi ASYROFI <mhilmiasyrofi@gmail.com>
2021-02-08 15:22:49 +0800

    add test data as a parameter

--- Bag-of-Tricks-for-AT/ensemble_model.sh
+++
@@ -1,8 +0,0 @@
-python ensemble_model.py  \
-    --model resnet18 \
-    --centroids pixelattack_spatialtransformation_autoattack
-
-# python ensemble_model.py  \
-#     --model resnet18 \
-#     --centroids pixelattack_spatialtransformation_squareattack_autopgd \
-#     --noise-predictor maxlr0.05_wd0.0001_ls0.3
